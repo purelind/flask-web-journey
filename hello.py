7bb01f5314b1d857e246c381deff29b83f4252b6
purelind <kevinsocial@outlook.com>
2017-07-08 21:13:39 +0800

    form handling in view functions

--- hello.py
+++ hello.py
@@ -1,16 +1,24 @@
-from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 # the only  required argument to the flask  class constructor is the name of the
 # main module or package of the application. For most applicaions, Python's __name__
 # variable is the correct value
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -21,15 +29,14 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 # a route is declared using this decorator
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 if __name__== '__main__':
