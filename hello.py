a57d80241fc0491fa34d55da8c0dc7a1b856b2f6
purelind <kevinsocial@outlook.com>
2017-07-08 17:15:16 +0800

    custom error pages

--- hello.py
+++ hello.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 # a route is declared using this decorator
 @app.route('/')
 def index():
