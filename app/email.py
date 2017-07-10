d1d8b57cde6c176ede8d5b22a371cf74f79573a5
purelind <kevinsocial@outlook.com>
2017-07-10 21:24:39 +0800

    chapter7 large application structure

--- app/email.py
+++ app/email.py
@@ -4,18 +4,18 @@ from flask_mail import Message
 from . import mail
 
 
-def send_async_eamil(app, msg):
+def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message(app.config['FLASKY_MAIl_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIl_SENDER'], recipients=[to])
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_eamil, args=[app, msg])
+    thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
 
--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
-    FLASKY_MAIL_SENDER = 'FLASKY Admin <flasky@example.com>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
@@ -33,7 +33,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
